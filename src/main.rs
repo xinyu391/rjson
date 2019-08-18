2fdb0057d77c226f97911ee404548f469bbfecb4
mskiller <mskiller@outlook.com>
2019-08-18 21:41:05 +0800

    parse obj

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,65 @@
-use std::io;
 use std::fs::File;
-use std::io::BufReader;
 use std::io::prelude::*;
+use std::collections::HashMap;
 
+#[derive(Debug)]
+enum JValue{
+    Bool(bool),
+    String(String),
+    Integer(i32),
+    Float(f64),
+    Array(Vec<JValue>),
+    Object(JObject),
+}
+#[derive(Debug)]
+struct JObject{
+    items : HashMap<String,JValue>,
+}
+impl JObject{
+    fn new()->JObject{
+        JObject{items:HashMap::new()}
+    }
+}
 fn main() {
     println!("Hello, world!");
     let path: &str = "sample.json";
-    println!("file {}",path);
-    let mut input :File = File::open(path).expect("No file opened!");
-    let mut content :String = String::new();
+    println!("file {}", path);
+    let mut input: File = File::open(path).expect("No file opened!");
+    let mut content: String = String::new();
     input.read_to_string(&mut content);
 
-    let mut chars  = content.chars().peekable();
-    // for c in chars{
-    //     print!("{}",c)
-    // }
-    
+    let mut chars = content.chars().peekable();
+
+    let mut obj :JObject;
+    // println!("{:?}",obj);
     loop {
-        let t =  read_token(&mut chars);
-    
-        if let Token::None = t{
-            break
-        }else{ 
-            
+        let t = read_token(&mut chars);
+
+        if let Token::None = t {
+            break;
+        } else {
             println!("{:?}", t);
+
+            if let Token::StartObj=t{
+                obj = JObject::new();
+            }
+            if let Token::String(s)=t{
+                // key s
+                let key = s;
+                let t = read_token(&mut chars);
+                if let Token::Colon =t {
+                    let t = read_token(&mut chars);
+                    // value t
+                    println!("{}:{:?}",key,t);
+                }
+            }
         }
     }
     println!();
 }
 #[derive(Debug)]
-enum Token{
-    Word(String),
+enum Token {
+    String(String),
     StartObj,
     EndObj,
     Comma,
@@ -44,60 +73,69 @@ enum Token{
     None,
 }
 
-fn read_token(chars :&mut std::iter::Peekable<std::str::Chars>)->Token{
-   
+fn read_token(chars: &mut std::iter::Peekable<std::str::Chars>) -> Token {
     // 过滤　空白符号
     // let p = chars.peek();
-    while  let Some(c) = chars.peek(){
+    while let Some(c) = chars.peek() {
         match c {
             ' ' | '\n' | '\r' | '\t' => chars.next(),
             _ => break,
         };
     }
-     let mut word = String::new();
-    let mut record =false;
-    while let  Some(&c) =  chars.peek(){
-           
-            if record{
-                 chars.next();
-                if c =='"'{
-                    return Token::Word(word);
-                }
-                word.push(c);
-                continue;
+    let mut word = String::new();
+    let mut record = false;
+    while let Some(&c) = chars.peek() {
+        if record {
+            chars.next();
+            if c == '"' {
+                return Token::String(word);
             }
-            if word.len()>0{
-                match c{
-                    ','|'}'|']' |' '|'\n'|'\r'|'\t'=>return Token::Word(word),
-                    _ =>(),
+            word.push(c);
+            continue;
+        }
+        if word.len() > 0 {
+            match c {
+                ',' | '}' | ']' | ' ' | '\n' | '\r' | '\t' => {
+                    if word == "true" {
+                        return Token::Bool(true);
+                    }
+                    if word == "false" {
+                        return Token::Bool(false);
+                    }
+                    return Token::Number(word);
                 }
+                _ => (),
             }
-             chars.next();
-            match c{
-                '{' => return Token::StartObj,
-                '}'=>return Token::EndObj,
-                '['=>return Token::StartArray,
-                ']'=>return Token::EndArray,                
-                ','=>return Token::Comma,
-                ':'=> return Token::Colon,
-                '"'=> if record{
-                    return Token::Word(word);
-                }else{
+        }
+        chars.next();
+        match c {
+            '{' => return Token::StartObj,
+            '}' => return Token::EndObj,
+            '[' => return Token::StartArray,
+            ']' => return Token::EndArray,
+            ',' => return Token::Comma,
+            ':' => return Token::Colon,
+            '"' => {
+                if record {
+                    return Token::String(word);
+                } else {
                     record = true;
                 }
-                '\n' => (),
-                _ => //if record{
-                    word.push(c),
-               // }
             }
-            //println!("{}",c);
-        
+            '\n' => (),
+            _ =>
+            //if record{
+            {
+                word.push(c)
+            } // }
+        }
+        //println!("{}",c);
     }
-    if word.len() >0{
-        if word =="true"{
+    if word.len() > 0 {
+        if word == "true" {
             return Token::Bool(true);
         }
-        if  word =="false"{
+        if word == "false" {
             return Token::Bool(false);
         }
         return Token::Number(word);
@@ -105,3 +143,5 @@ fn read_token(chars :&mut std::iter::Peekable<std::str::Chars>)->Token{
     println!("{}", word);
     Token::None
 }
+
+
